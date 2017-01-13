4091b47dab3e87d9f54cfaaf96db11f384361228
Andrew Kozin (aka nepalez) <andrew.kozin@gmail.com>
2017-01-13 23:04:45 +0300

    Fix style

--- spec/tram/examiner_spec.rb
+++ spec/tram/examiner_spec.rb
@@ -19,29 +19,29 @@ describe Tram::Examiner do
   end
 
   it "doesn't include ActiveModel::Validations into the class" do
-    expect(Test::User.new('', '')).not_to be_a ActiveModel::Validations
+    expect(Test::User.new("", "")).not_to be_a ActiveModel::Validations
   end
 
   context "#valid?" do
     it "works" do
-      expect(Test::User.new('', '')).not_to be_valid
-      expect(Test::User.new('Andy', '')).to be_valid
+      expect(Test::User.new("", "")).not_to be_valid
+      expect(Test::User.new("Andy", "")).to be_valid
     end
   end
 
   context "#errors" do
     it "works" do
-      expect(Test::User.new('', '').errors).not_to be_empty
-      expect(Test::User.new('Andy', '').errors).to be_empty
+      expect(Test::User.new("", "").errors).not_to be_empty
+      expect(Test::User.new("Andy", "").errors).to be_empty
     end
   end
 
   context "#validate!" do
     it "works" do
-      expect { Test::User.new('', '').validate! }
+      expect { Test::User.new("", "").validate! }
         .to raise_error ActiveModel::ValidationError
 
-      expect { Test::User.new('Andy', '').validate! }
+      expect { Test::User.new("Andy", "").validate! }
         .not_to raise_error
     end
   end
